a0d1b1fe5f86c7145fa4daf4a4c14d6150505e87
jstabi01 <joeystabi01@gmail.com>
2022-07-06 17:02:18 -0400

    Updates

---
+++ precache-manifest.d82f990e29b47527ff1c1448346c43ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c23c27cc068fdbaab69",
+    "url": "/four-app/static/css/main.5c24f9e6.chunk.css"
+  },
+  {
+    "revision": "3c23c27cc068fdbaab69",
+    "url": "/four-app/static/js/main.3c23c27c.chunk.js"
+  },
+  {
+    "revision": "e142c7c0daf42d783361",
+    "url": "/four-app/static/js/1.e142c7c0.chunk.js"
+  },
+  {
+    "revision": "c364b1a81cb318f2bfe6",
+    "url": "/four-app/static/js/2.c364b1a8.chunk.js"
+  },
+  {
+    "revision": "b70f6aa6629d2c86fefc",
+    "url": "/four-app/static/js/runtime~main.b70f6aa6.js"
+  },
+  {
+    "revision": "ff04d424b6a2c9aa105d34bc78cea234",
+    "url": "/four-app/index.html"
+  }
+];
